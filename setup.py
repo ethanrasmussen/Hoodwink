06a61812159d6eb54d43922e67e3e652881cbf40
ethanrasmussen <59754559+ethanrasmussen@users.noreply.github.com>
2020-04-21 11:31:29 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setuptools.setup(
+    name = "Hoodwink",
+    version = "0.0.1",
+    author = "Ethan M. Rasmussen",
+    author_email = "ethan@razgroup.com",
+    description = "A simple package for snatching basic stock data from Robinhood.",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/ethanrasmussen/hoodwink",
+    packages = setuptools.find_packages(),
+)
