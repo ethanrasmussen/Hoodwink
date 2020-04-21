50b7b8831b80507a47800e85c1757d5ebcac4a90
ethanrasmussen <59754559+ethanrasmussen@users.noreply.github.com>
2020-04-21 11:31:51 -0500

    Add files via upload

---
+++ hoodwink/hoodwink.py
@@ -0,0 +1,56 @@
+#imports
+
+#NOTE: data may be slightly delayed in current version of Hoodwink, as logging-in is not yet supported!
+
+#get full name from ticker symbol
+def getFullName(ticker):
+    pass
+
+#get description
+def getDescription(ticker):
+    pass
+
+#get list of tags/collections
+def getCollections(ticker):
+    pass
+
+#get number of owners of stock on RH
+def getOwnerCount(ticker):
+    pass
+
+#get current price
+def getPrice(ticker):
+    pass
+
+#get price change in USD (periods: daily, weekly, monthly, 3-month, annual, 5-year)
+def getNumericChange(ticker, period="daily"):
+    pass
+
+#get percentage price change (periods: daily, weekly, monthly, 3-month, annual, 5-year)
+def getPercentChange(ticker, period="daily"):
+    pass
+
+#get market cap
+def getMarketCap(ticker):
+    pass
+
+#get today's open price
+def getOpen(ticker):
+    pass
+
+#get high (periods: today, 52w)
+def getHigh(ticker, period="today"):
+    pass
+
+#get low (periods: today, 52w)
+def getLow(ticker, period="today"):
+    pass
+
+#get P/E ratio
+def getPE(ticker):
+    pass
+
+#get dividend yield
+def getDivYield(ticker):
+    pass
+
